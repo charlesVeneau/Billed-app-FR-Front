4106c01821bde5e1584a09e74a31bd7845c51c7e
CharlesVeneau <veneauc@hotmail.fr>
2022-02-08 22:37:59 +0100

    Draft of NewBills test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -3,20 +3,57 @@
  */
 
 import { screen } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
 import NewBillUI from '../views/NewBillUI.js'
 import NewBill from '../containers/NewBill.js'
+import { localStorageMock } from '../__mocks__/localStorage.js'
+import { ROUTES } from '../constants/routes'
+import { bills } from '../fixtures/bills'
+
+let currentNewBill
+
+beforeEach(() => {
+  Object.defineProperty(window, 'localStorage', {
+    value: localStorageMock,
+  })
+  window.localStorage.setItem('user', JSON.stringify({ type: 'Employee' }))
+  const html = NewBillUI()
+  document.body.innerHTML = html
+  const onNavigate = (pathname) => {
+    document.body.innerHTML = ROUTES({ pathname })
+  }
+  const store = null
+  currentNewBill = new NewBill({
+    document,
+    onNavigate,
+    store,
+    localStorage: window.localStorage,
+  })
+})
 
 describe('Given I am connected as an employee', () => {
   describe('When I am on NewBill Page', () => {
-    test('Then the title of the page is Envoyer une note de frais', () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
+    test('Then the title of the page is set', () => {
       expect(screen.getByText('Envoyer une note de frais')).toBeTruthy()
       //to-do write assertion
     })
-    //check if the page is loaded
-    //check if you can add a jpg file
-    //check if you can't add a pdf file
-    //On submit of the form the bills page is loaded
+    //test the file input to see if it handles jpg file
+    //mock the handleChangeFile method
+    //set a jpeg file as the input target
+    //see if there is a window alert
+    test('Then I can select an image file, jpg, jpeg or png', () => {
+      const handleChangeFile = jest.fn((e) =>
+        currentNewBill.handleChangeFile(e)
+      )
+      const selectFile = screen.getByTestId('file')
+      const testFile = new File(['This is a test'], 'test.png', {
+        type: 'image/png',
+      })
+      selectFile.addEventListener('change', handleChangeFile)
+      userEvent.upload(selectFile, testFile)
+      console.log(selectFile.files[0])
+      expect(selectFile).toBeTruthy()
+    })
+    test("Then I can't select a pdf file", () => {})
   })
 })
